2a8e650ec587f480f14331358aa2678b4f2efb66
lizburkechi <liz.burke.chi@gmail.com>
2021-03-06 19:32:44 -0600

    completed except second bonus deliverable

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,38 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+ 
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+       employee = Employee.create(employee_params)
+       redirect_to employee_path(employee)        
+    end
+
+    def edit  
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        employee = Employee.find(params[:id])
+        employee_params
+    end
+    
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
+    
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, presence: true, uniqueness: true
+    validates :title, presence: true, uniqueness: true
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end 
+   
 end
